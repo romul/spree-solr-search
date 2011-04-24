2ab47174313ca90f91e7e687dc02a1bfed57a036
Denis Ivanov <visible.h4x@gmail.com>
2011-04-25 07:52:56 +0800

    Rescueing errors upon initial migrations [Table database.products not found]

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,5 @@
 Product.class_eval do
-  acts_as_solr :fields => PRODUCT_SOLR_FIELDS, :facets => PRODUCT_SOLR_FACETS
+  acts_as_solr :fields => PRODUCT_SOLR_FIELDS, :facets => PRODUCT_SOLR_FACETS rescue nil
 
   def taxon_ids
     taxons.map(&:id)
