e3689e946c00caef7b1eb96e4315bb6ce2fdb087
Roman Smirnov <roman@railsdog.com>
2011-05-29 21:23:23 +0400

    Switch to Deface

--- lib/spree_solr_search_hooks.rb
+++ lib/spree_solr_search_hooks.rb
@@ -1,6 +1,14 @@
-class SpreeSolrSearchHooks < Spree::ThemeSupport::HookListener
-  
-  insert_before :search_results, 'products/facets'
-  insert_before :search_results, 'products/suggestion'
-  
-end
+Deface::Override.new(
+  :virtual_path => "products/index",
+  :name => "solr_facets_in_search_results",
+  :insert_before => "[data-hook='search_results']",
+  :partial => "products/facets",
+  :disabled => false)
+
+Deface::Override.new(
+  :virtual_path => "products/index",
+  :name => "solr_suggestion_in_search_results",
+  :insert_before => "[data-hook='search_results']",
+  :partial => "products/suggestion",
+  :disabled => false)
+
